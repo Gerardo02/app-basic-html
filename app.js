e3f39bf36a657b1444c5c32f013073c89ef4fee2
gerardo jerez <gerardo.jerez@hotmail.com>
2019-08-26 09:13:26 -0700

    Empezamos js

--- app.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-=======
-alert('duuude js working');
-let btnDo = document.getElementById('btnDo');
-btnDo.addEventListener('click', ()=>{alert('hola')});
->>>>>>> bb496bcff3a09f24f90e9daf322cf09239138a51
-
