d94bae4536c89adeca2c52bdfdfb7e49421e45fd
gerardo jerez <gerardo.jerez@hotmail.com>
2019-08-27 10:07:48 -0700

    Suena la nota Do

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 const buttons = document.querySelectorAll('button');
+//const audio = document.getElementById("AudioDo");
+const sound = new Audio();
+sound.src = "audio/Do.mp3";
 
 const playnote = event =>{
     console.log(event.target.dataset.note);
+    
 }
-
 buttons.forEach(
     button => button.addEventListener('click', playnote)
+    
 );
